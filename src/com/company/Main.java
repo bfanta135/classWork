f8872a4bb5ea1b38e5862bcf278877bc729921eb
bemente fanta <bementefanta12@gmail.com>
2019-04-17 14:50:38 -0400

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import com.sun.org.apache.bcel.internal.generic.NEW;
+
 import java.util.Random;
 import java.util.Scanner;
 
@@ -9,27 +11,49 @@ public class Main {
         Scanner scan = new Scanner(System.in);
 
         //
-        Random random = new Random();
-        int from = 1;
-        int to = 100;
-        int randomNumber = random.nextInt(to - from + 1) + from;
-        //
-        int guessedNumber = 0;
+//        Random random = new Random();
+//        int from = 1;
+//        int to = 100;
+//        int randomNumber = random.nextInt(to - from + 1) + from;
+//        //
+//        int guessedNumber = 0;
+//
+//        //
+//        System.out.printf("The number is between %d and %d.\n", from, to);
+//
+//        //
+//        do
+//        {
+//            System.out.print("Guess what the number is: ");
+//            guessedNumber = scan.nextInt();
+//            if (guessedNumber > randomNumber)
+//                System.out.println("Your guess is too high!");
+//            else if (guessedNumber < randomNumber)
+//                System.out.println("Your guess is too low!");
+//            else
+//                System.out.println("You got it!");
+//        } while (guessedNumber != randomNumber);
+        String firstIntial, lastName,streetName,streetType,city;
+        int houseNumber;
 
-        //
-        System.out.printf("The number is between %d and %d.\n", from, to);
+        System.out.print("enter first intial:");
+        firstIntial = scan.nextLine();
+        System.out.print("enter last name:");
+        lastName = scan.nextLine();
+        System.out.print("enter house number:");
+        houseNumber = scan.nextInt();
+        scan.nextLine();
+        System.out.print("enter street name:");
+        streetName = scan.nextLine();
+        System.out.print("enter street type:");
+        streetType = scan.nextLine();
+        System.out.print("ente city:");
+        city = scan.nextLine();
 
-        //
-        do
-        {
-            System.out.print("Guess what the number is: ");
-            guessedNumber = scan.nextInt();
-            if (guessedNumber > randomNumber)
-                System.out.println("Your guess is too high!");
-            else if (guessedNumber < randomNumber)
-                System.out.println("Your guess is too low!");
-            else
-                System.out.println("You got it!");
-        } while (guessedNumber != randomNumber);
+
+
+
+        System.out.println(firstIntial + "" + lastName + " " + houseNumber + " ");
+        System.out.println(streetName + " " + streetType + " " + city + "");
     }
 }
